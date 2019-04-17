0c8ae29c39f2bc956c1e82211178f43366aa9f1e
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2019-04-17 13:22:09 -0300

    rednose and coverage

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-
+test_requires = [
+    'nose==1.3.7',
+    'coverage==4.5.3',
+    'rednose==1.3.0'
+]
+    
 setup(
     name='hunor',
     description='The son of Nimrod',
@@ -33,11 +38,7 @@ setup(
         'peewee==3.7.1'
     ],
     test_suite='nose.collector',
-    tests_require=[
-        'nose==1.3.7',
-        'coverage==4.5.3',
-        'rednose==1.3.0'
-    ],
+    tests_require=test_requires,
     include_package_data=True,
     zip_safe=False,
     entry_points={
@@ -54,6 +55,6 @@ setup(
     ],
     python_requires='>=3',
     setup_requires=[
-        'nose==1.3.7'
-    ]
+
+    ] + test_requires
 )
