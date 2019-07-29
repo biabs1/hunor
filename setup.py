a0bc9b3eb5b66501c4d402540df06e1275e07b58
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2019-07-29 17:08:30 -0300

    hunor@0.9.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description='The son of Nimrod',
     long_description=readme(),
     keywords='test mutant analysis equivalent',
-    version='0.9.5',
+    version='0.9.6',
     url='https://github.com/marcioaug/hunor',
     author='Marcio Augusto Guimarães',
     author_email='masg@ic.ufal.br',
