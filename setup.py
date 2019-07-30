6f0827349b8681db005901b94c6f502b5a40c5de
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2019-07-29 21:47:34 -0300

    update hunor-maven-plugin to 0.3.4.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description='The son of Nimrod',
     long_description=readme(),
     keywords='test mutant analysis equivalent',
-    version='0.9.6',
+    version='0.9.7',
     url='https://github.com/marcioaug/hunor',
     author='Marcio Augusto Guimarães',
     author_email='masg@ic.ufal.br',
